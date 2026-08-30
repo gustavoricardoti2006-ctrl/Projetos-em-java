62daea697cd63ee78325c6444a6146b94f6f80d5
gustavoricardoti2006-ctrl <gustavoricardoti2006@gmail.com>
2026-08-30 12:43:39 -0300

    t

---
+++ trabalho03/exer1.java
@@ -0,0 +1,16 @@
+import java.util.scanner;
+
+public class exer1{
+    public static void main(String[] arg){
+        scanner sc = new Scanner(System.in);
+
+        System.out.printf("Diga o numero: ");
+        int num = sc.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+            System.out.printf(num + "*" + i "=" + (num * i));
+        }
+
+
+    }
+}
