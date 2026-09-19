ae23ac235024d284c361bd4eed81e5add3cf7440
gustavoricardoti2006-ctrl <gustavoricardoti2006@gmail.com>
2026-09-19 20:14:07 -0300

    aa

--- Trabalho07/exer1/controle/main.java
+++ Trabalho07/exer1/controle/main.java
@@ -1,4 +1,16 @@
 package trabalho07.exer01.dominio.Produto;
 import java.text.Scanner;
 
+class public main(){
+    public void static main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        Produto a = new Produto("batata", 0.88, 130);
+        Produto b = new Produto("Tomatea", 0.30, 500);
+
+
+        System.out.printf("Nome produto: %s", a.getNome());
+    }
+}
+
 
